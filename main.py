a24e54aaa4bc7bbfabe41a308f7d44ead3c35cc7
Tim Rekelj <tim.rekelj@proton.me>
2023-11-02 20:59:19 +0100

    testing commit #7

--- main.py
+++ main.py
@@ -164,25 +164,27 @@ def build_list_of_folders(
         )
     )
 
-    if (arg[-1] == os.sep or arg == '') and folder.exists():
-        for folder in folders[:limit_folders_to_show]:
-            try_relative_folder = str(folder)
-            if try_relative_folder.startswith(home):
-                try_relative_folder = try_relative_folder.replace(home, '', 1)
+    for folder in folders[:limit_folders_to_show]:
+        try_relative_folder = str(folder)
+        if try_relative_folder.startswith(home):
+            try_relative_folder = try_relative_folder.replace(home, '', 1)
 
-            items.append(
-                ExtensionResultItem(
-                    icon='images/inner-folder.png',
-                    name=try_relative_folder,
-                    description=f'VSCode folder: {folder}',
-                    on_enter=ActionList([
-                        SetUserQueryAction(
-                            f'{vs_keyword} {try_relative_folder}{os.sep}'
-                        ),
-                        ExtensionCustomAction(folder, keep_app_open=True)
-                    ]),
-                )
+        items.append(
+            ExtensionResultItem(
+                icon='images/inner-folder.png',
+                name=try_relative_folder,
+                description=f'VSCode folder: {folder}',
+                on_enter=ActionList([
+                    SetUserQueryAction(
+                        f'{vs_keyword} {try_relative_folder}{os.sep}'
+                    ),
+                    ExtensionCustomAction(folder, keep_app_open=True)
+                ]),
             )
+        )
+
+    if (arg[-1] == os.sep or arg == '') and folder.exists():
+        items.pop(0)
 
     return items
 
